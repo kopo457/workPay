bd8a2a266ce50c93233ea68207e017ffacc9f171
Kim  Minjun <minjun457457@naver.com>
2023-05-30 01:20:33 +0900

    일단 총 근무 시간만 보이도록 만듬

--- workPay.js
+++ workPay.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
   let dayNumber = getDayNum();
   let calendar = createCalendar(dayNumber.year, dayNumber.month);
   let calendarElement = document.getElementById('calendar');
@@ -11,12 +11,12 @@ function getDayNum() {
   let month = today.getMonth();
   let date = today.getDate();
   let day = today.getDay();
-  (day -1 == -1) ? day = 6 : day -= 1;
+  (day - 1 == -1) ? day = 6 : day -= 1;
 
   let firstDay = new Date(year, month, 1);
   let firstDate = firstDay.getDate();
   let firstDayNum = firstDay.getDay();
-  (firstDayNum -1 == -1) ? firstDayNum = 6 : firstDayNum -= 1;
+  (firstDayNum - 1 == -1) ? firstDayNum = 6 : firstDayNum -= 1;
 
   let lastDay = new Date(year, month + 1, 0);
   let lastDate = lastDay.getDate();
@@ -48,7 +48,7 @@ function createCalendar(year, month) {
 
   //요일 표시
   let header = calendar.insertRow();
-  for(let dayName = 0; dayName < 7; dayName++) {
+  for (let dayName = 0; dayName < 7; dayName++) {
     let cell = header.insertCell();
     let dayText = document.createElement('p');  //요일 넣을 p태그 생성
     dayText.textContent = weekdays[dayName];    //p태그 안에 요일 넣기
@@ -56,11 +56,8 @@ function createCalendar(year, month) {
     cell.appendChild(dayText);                  //p태그를 셀의 자식으로 추가
     cell.classList.add('dayCell');
 
-    if (dayName === 5) {
-      cell.classList.add('saturdayName');
-    } else if (dayName === 6) {
-      cell.classList.add('sundayName');
-    }
+    if (dayName === 5) { cell.classList.add('saturdayName'); }
+    else if (dayName === 6) { cell.classList.add('sundayName'); }
   }
 
   //이번 달 첫 요일 구하기
@@ -78,7 +75,7 @@ function createCalendar(year, month) {
     let dateText = document.createElement('p');
     dateText.textContent = date.getDate();
     cell.appendChild(dateText);
-    
+
     if (dayCount === 5) {                   // 토요일인 경우 'Saturday' 클래스 추가
       dateText.classList.add('saturday');
     } else if (dayCount === 6) {            // 일요일인 경우 'Sunday' 클래스 추가
@@ -86,17 +83,17 @@ function createCalendar(year, month) {
     } else {                                // 나머지 요일인 경우 'dateNumber' 클래스 추가
       dateText.classList.add('dateNumber');
     }
-    
+
     // 날짜 입력을 위한 input 요소 생성
-    let dateInput = document.createElement('input');  
+    let dateInput = document.createElement('input');
     dateInput.type = 'text';
     dateInput.classList.add('dateInput');
     dateInput.size = 1;
     dateInput.step = 0.1;
-    dateInput.onkeyup = function(){sumWorkTime()};
+    dateInput.onkeyup = function() { sumWorkTime() };
     cell.appendChild(dateInput);
     date.setDate(date.getDate() + 1);
-  
+
     // 일요일이면 다음 행으로 이동
     if (dayCount < 6) {
       dayCount++;
@@ -109,6 +106,7 @@ function createCalendar(year, month) {
   //마지막 주 빈 셀 채우기
   while (row.cells.length < 7) {
     let emptyCell = row.insertCell();
+    emptyCell.classList.add("emptyCell");
   }
 
   return calendar;
@@ -116,11 +114,43 @@ function createCalendar(year, month) {
 
 function sumWorkTime() {
   let sumWorkTime = 0;
-  for(let i = dayNum.firstDayNum; i < dayNum.lastDate; i++) {
+  let sumWeekTime = [0, 0, 0, 0, 0, 0];
+  let j = 0;
+  let dateCount = dayNum.firstDayNum;
+
+  for (let i = dayNum.firstDayNum; i < dayNum.lastDate; i++) {
     let dayWorkTime = document.getElementsByClassName("dateInput")[i].value;
     sumWorkTime += Number(dayWorkTime);
+    if (dateCount < 6) {
+      sumWeekTime[j] += Number(dayWorkTime);
+      dateCount++;
+    } else if (dateCount === 6) {
+      sumWeekTime[j] += Number(dayWorkTime);
+      j++;
+      dateCount = 0;
+    }
+
     let printWorkTime = document.getElementById('resultP');
     printWorkTime.textContent = "총 근무 시간은 " + sumWorkTime + "시간 입니다.";
   }
-  console.log(sumWorkTime);
+  return {
+    sumWorkTime: sumWorkTime,
+    week1: sumWeekTime[0],
+    week2: sumWeekTime[1],
+    week3: sumWeekTime[2],
+    week4: sumWeekTime[3],
+    week5: sumWeekTime[4],
+    week6: sumWeekTime[5]
+  }
+}
+
+function resultPay() {
+  let pay = Number(document.getElementById('pay').value);
+  let sumpay = sumWorkTime().sumWorkTime * pay;
+  let payFormat = sumpay.toLocaleString();
+
+  if (pay >= 9620) {
+    let sumWorkPay = document.getElementById('sumWorkPay');
+    sumWorkPay.textContent = payFormat;
+  }
 }
